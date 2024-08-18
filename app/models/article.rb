ae0f8552ca918f13cd1ccc3a35acf34a1a4ee653
saj33l <sajeel.khan01@gmail.com>
2024-08-19 02:29:01 +0500

    new columns added

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,13 @@
 class Article < ApplicationRecord
   has_many :comments
 
+  VALID_STATUSES = %w[public private archived]
+
+  validates :status, inclusion: { in: VALID_STATUSES }
   validates :title, presence: true
   validates :body, presence: true, length: { minimum: 10 }
+
+  def archived?
+    status == "archived"
+  end
 end
