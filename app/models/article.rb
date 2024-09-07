b94697f9f5a62880b4c1014d6f9162539b13b946
saj33l <sajeel.khan01@gmail.com>
2024-09-07 18:13:53 +0500

    added concerns

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,13 +1,8 @@
 class Article < ApplicationRecord
+  include Visible
+
   has_many :comments
 
-  VALID_STATUSES = %w[public private archived]
-
-  validates :status, inclusion: { in: VALID_STATUSES }
   validates :title, presence: true
   validates :body, presence: true, length: { minimum: 10 }
-
-  def archived?
-    status == "archived"
-  end
 end
